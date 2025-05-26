52832f88e0b4d637a649a87ad75f063a824b4ecf
Bernardo Simões <151968699+b3rnardo15@users.noreply.github.com>
2025-05-26 16:31:28 +0000

    ajuste, bastava ler

--- components/AreaCard.tsx
+++ components/AreaCard.tsx
@@ -33,8 +33,8 @@ const getIconFilename = (areaName: string): string => {
   if (normalizedName.includes('performance')) return 'performance-icone.png'; 
   if (normalizedName.includes('retencao')) return 'retencao-icone.png';
   if (normalizedName.includes('rh')) return 'rh-icone.png';
-  if (normalizedName.includes('rh')) return 'financeiro-icone.png';
-  if (normalizedName.includes('rh')) return 'jurídico-icone.png';
+  if (normalizedName.includes('financeiro')) return 'financeiro-icone.png';
+  if (normalizedName.includes('jurídico')) return 'jurídico-icone.png';
 
   // Add specific checks for 'Comercial - Individual' and 'Comercial Interno' if needed
   // Example: if (normalizedName === 'comercial individual') return 'some-icon.png';
